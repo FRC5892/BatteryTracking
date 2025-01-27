ec6fcebed75b1e32db65aa6c0846b633f8d6f9b0
Michael <117385299+mstatistics@users.noreply.github.com>
2025-01-26 19:24:46 -0600

    Idk it works now

---
+++ src/main/java/org/team5892/BatteryTracking/Main.java
@@ -0,0 +1,41 @@
+package org.team5892.BatteryTracking;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTablesJNI;
+import edu.wpi.first.util.CombinedRuntimeLoader;
+
+import java.io.IOException;
+
+public class Main {
+    private static Main instance;
+    public static Main getInstance() {
+        if (instance == null) {
+            instance = new Main();
+        }
+        return instance;
+    }
+    public static void main(String[] args) throws IOException {
+        Main.getInstance().start(args);
+    }
+    public void start(String... args) throws IOException {
+        NetworkTablesJNI.Helper.setExtractOnStaticLoad(false);
+        CombinedRuntimeLoader.loadLibraries(Main.class, "wpiutiljni", "ntcorejni");
+        if (args.length == 0) {
+            System.out.println("Usage: java -jar BatteryTracking.jar <team/ip/localhost>");
+            return;
+        }
+        NetworkTableInstance instance = NetworkTableInstance.getDefault();
+        try {
+            int team = Integer.parseInt(args[0]);
+            instance.setServerTeam(team);
+        } catch (NumberFormatException e) {
+            instance.setServer(args[0]);
+        }
+        instance.startClient4("Battery Tracking");
+        NetworkTable table = instance.getTable("BatteryTracking");
+        table.getIntegerTopic("write")
+
+    }
+    protected static class NTErrors
+}
